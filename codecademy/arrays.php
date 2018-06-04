da39abba43dd1f2ed0a7fdb6deef9e464bc54f16
ChristopherALee <christopheradamlee@gmail.com>
2018-06-04 18:08:46 -0400

    Completed arrays

---
+++ codecademy/arrays.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Woot, Arrays!</title>
+  </head>
+  <body>
+    <?php
+      // declaring an array
+      $array = array("Egg", "Tomato", "Beans");
+
+      // deleting from array
+      unset($array[1]);
+    ?> 
+  </body>
+</html>
